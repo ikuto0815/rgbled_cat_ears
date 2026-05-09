77618e1ed99430dbb2f6afa9d71e1c8a6e8720f1
ikuto0815 <ikuto0815@proton.me>
2026-05-10 00:24:20 +0200

    ble: move characteristics to defines
    That way they can be used outside of the Ble object.
    (like in the callback for the ble characteristic)

--- include/ble.h
+++ include/ble.h
@@ -4,13 +4,15 @@
 
 #include <BLECharacteristic.h>
 
+#define CONFIG_SERVICE_UUID "5c538829-50d8-4b0b-9b80-fee97a609e1f"
+#define CHARACTERISTIC_BRIGHTNESS_UUID "5c538829-50d8-4b0b-9b80-fee97a600000"
+#define CHARACTERISTIC_COLOR_UUID "5c538829-50d8-4b0b-9b80-fee97a600001"
+#define CHARACTERISTIC_MODE_UUID "5c538829-50d8-4b0b-9b80-fee97a600002"
+
+#define CUSTOM_DESCRIPTOR_UUID "2b6fface-0815-4454-be5a-334af5c5f118"
+
 class Ble {
 	private:
-		const char *CONFIG_SERVICE_UUID = "5c538829-50d8-4b0b-9b80-fee97a609e1f";
-		const char *CHARACTERISTIC_BRIGHTNESS_UUID = "5c538829-50d8-4b0b-9b80-fee97a600000";
-		const char *CHARACTERISTIC_COLOR_UUID = "5c538829-50d8-4b0b-9b80-fee97a600001";
-
-		const char *CUSTOM_DESCRIPTOR_UUID = "2b6fface-0815-4454-be5a-334af5c5f118";
 		
 		void createVariableCharacteristic(BLEService *pService, void *var, size_t size, const char *uuid, const String &descriptor);
 
