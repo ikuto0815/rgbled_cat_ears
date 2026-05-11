faf7b86e84b20ce999a54c8c4c0bc01fa8cf014a
ikuto0815 <ikuto0815@proton.me>
2026-05-12 00:09:05 +0200

    ExpFilter: split up into h and cpp file

--- include/ExpFilter.h
+++ include/ExpFilter.h
@@ -7,6 +7,7 @@
  */
 
 #pragma once
+#include <inttypes.h>
 
 class ExpFilter {
     private:
@@ -23,67 +24,3 @@ class ExpFilter {
 	void update0(float *data);
 	float *value();
 };
-
-ExpFilter::ExpFilter(float *val, int val_len, float alpha_decay, float alpha_rise)
-{
-	m_val_len = val_len;
-	m_alpha_decay = alpha_decay;
-	m_alpha_rise = alpha_rise;
-
-	m_val = new float[val_len];
-	for (int i = 0; i < m_val_len; i++)
-		m_val[i] = val[i];
-}
-
-ExpFilter::ExpFilter(int val_len, float alpha_decay, float alpha_rise)
-{
-	m_val_len = val_len;
-	m_alpha_decay = alpha_decay;
-	m_alpha_rise = alpha_rise;
-
-	m_val = new float[val_len];
-	for (int i = 0; i < m_val_len; i++)
-		m_val[i] = 0.0;
-}
-
-ExpFilter::~ExpFilter()
-{
-	delete[] m_val;
-}
-
-float *ExpFilter::value()
-{
-	return m_val;
-}
-
-void ExpFilter::update0(float *data)
-{
-	for (int i = 0; i < m_val_len; i++) {
-		if (data[i] > m_val[i])
-			m_val[i] = data[i] * m_alpha_rise + m_val[i] * (1.0 - m_alpha_rise);
-		else
-			m_val[i] = data[i] * m_alpha_decay + m_val[i] * (1.0 - m_alpha_decay);
-	}
-}
-
-void ExpFilter::update(float *data)
-{
-	for (int i = 0; i < m_val_len; i++) {
-		if (data[i] > m_val[i])
-			m_val[i] = data[i] * m_alpha_rise + m_val[i] * (1.0 - m_alpha_rise);
-		else
-			m_val[i] = data[i] * m_alpha_decay + m_val[i] * (1.0 - m_alpha_decay);
-		data[i] = m_val[i];
-	}
-}
-
-void ExpFilter::update(uint8_t *data)
-{
-	for (int i = 0; i < m_val_len; i++) {
-		if (data[i] > m_val[i])
-			m_val[i] = data[i] * m_alpha_rise + m_val[i] * (1.0 - m_alpha_rise);
-		else
-			m_val[i] = data[i] * m_alpha_decay + m_val[i] * (1.0 - m_alpha_decay);
-		data[i] = m_val[i];
-	}
-}
